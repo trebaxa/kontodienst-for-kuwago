95cc0923914efb4a4dd732bb19f72a82512ba6e1
trebaxa <service@trebaxa.com>
2023-02-27 10:32:05 +0100

    Update debug mode

--- install.php
+++ install.php
@@ -1,10 +1,9 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('track_errors', '1');
+ini_set('log_errors_max_len', '1024');
+error_reporting(E_ALL ^ E_NOTICE);
 
-error_reporting(E_ALL);
- ini_set('display_errors', '1');
-            ini_set('track_errors', '1');
-            ini_set('log_errors_max_len', '1024');
-            error_reporting(E_ALL ^ E_NOTICE);
 require ("classes/kd_master.class.php");
 require ('classes/kd_install.class.php');
 
